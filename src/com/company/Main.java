fa39b5c6caa5cb0e0b5b88b438ffd654cd7db765
Your Name <Kristiyan Bukov>
2020-01-17 23:49:29 +0200

    First Develop Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        // This is develop mode
         SnakesLadders game = new SnakesLadders();
-
         System.out.println(game.play(1, 1));
         System.out.println(game.play(1, 5));
         System.out.println(game.play(6, 2));
